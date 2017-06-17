c8b0ec25231cb48969ec459e33e38e4a281bee15
Conan Tzou <conan@asperasoft.com>
2017-06-17 12:11:03 -0700

    create another json file for testing

--- app/directory/controller.js
+++ app/directory/controller.js
@@ -6,8 +6,7 @@ function directoryCtrl (DataService) {
   this.handleFileSelect = ev => {
     let file = ev.target.files[0];
     DataService.readData(file).then(data => {
-      console.log('json', JSON.parse(data));
-      // this.data = JSON.parse(data);
+      this.data = JSON.parse(data);
     });
   };
 
