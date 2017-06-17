f7fe2dd92ce7c021a6d233e28713e827286a580e
Conan Tzou <conan@asperasoft.com>
2017-06-17 11:58:10 -0700

    validate json format

--- app/directory/controller.js
+++ app/directory/controller.js
@@ -6,7 +6,6 @@ function directoryCtrl (DataService) {
   this.handleFileSelect = ev => {
     let file = ev.target.files[0];
     DataService.readData(file).then(data => {
-      console.log('dataread', data);
       console.log('json', JSON.parse(data));
       // this.data = JSON.parse(data);
     });
